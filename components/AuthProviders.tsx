fa54c4fd388ba38e2adc1a7c3e166132d3627e89
UGJavascript <ugfrontier97@gmail.com>
2023-06-30 18:22:24 +0500

    Dribble

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,4 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getProviders, signIn } from 'next-auth/react';
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
   return <div>AuthProviders</div>;
